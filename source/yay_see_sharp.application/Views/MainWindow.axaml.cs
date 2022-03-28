5451acacc208b49a027a70c58874f03f6637df1b
Stipec <stipec502@gmail.com>
2022-03-29 00:30:05 +0200

    *removing icon is not needed

--- source/yay_see_sharp.application/Views/MainWindow.axaml.cs
+++ source/yay_see_sharp.application/Views/MainWindow.axaml.cs
@@ -4,8 +4,6 @@ namespace yay_see_sharp.application.Views
 {
     public partial class MainWindow : Window
     {
-        private WindowIcon myIcon;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -15,15 +13,12 @@ namespace yay_see_sharp.application.Views
         {
             if (state == WindowState.Minimized)
             {
-                myIcon = Icon;
-                Icon = null;
                 ShowInTaskbar = false;
                 Hide();
             }
 
             if(state == WindowState.Normal)
             {
-                Icon = myIcon;
                 ShowInTaskbar = true;
                 this.BringIntoView();
                 Activate();
