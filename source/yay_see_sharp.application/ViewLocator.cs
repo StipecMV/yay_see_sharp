8b4c85f697961907f1782de96ea27f3ad49623b5
Stipec <93717393+StipecMV@users.noreply.github.com>
2026-07-31 12:21:49 +0200

    chore: modernize project structure and target .NET 10

--- source/yay_see_sharp.application/ViewLocator.cs
+++ source/yay_see_sharp.application/ViewLocator.cs
@@ -7,8 +7,13 @@ namespace yay_see_sharp.application
 {
     public class ViewLocator : IDataTemplate
     {
-        public IControl Build(object data)
+        public Control? Build(object? data)
         {
+            if (data is null)
+            {
+                return null;
+            }
+
             var name = data.GetType().FullName!.Replace("ViewModel", "View");
             var type = Type.GetType(name);
 
@@ -22,7 +27,7 @@ namespace yay_see_sharp.application
             }
         }
 
-        public bool Match(object data)
+        public bool Match(object? data)
         {
             return data is ViewModelBase;
         }
